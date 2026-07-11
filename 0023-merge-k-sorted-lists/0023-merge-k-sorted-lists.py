32b70da64645bc3eae6d478fd05d3ebd23f8fe9f
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-07-11 23:41:59 +0530

    Time: 8 ms (70.26%), Space: 22.5 MB (98%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,28 +1,36 @@
+from typing import List, Optional
+
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import heapq
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[Optional[ListNode]]
-        :rtype: Optional[ListNode]
-        """
-        heap =[]
 
-        for head in lists:
-            while head:
-                heapq.heappush(heap,head.val)
-                head= head.next 
-        
-        if not heap :
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
             return None
-               
-        head = ListNode(heapq.heappop(heap))
-        curr = head 
-        while heap :
-            curr.next = ListNode(heapq.heappop(heap))
-            curr = curr.next 
-        return head 
+        if len(lists) == 1:
+            return lists[0]
+
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        curr.next = l1 or l2
+        return dummy.next
