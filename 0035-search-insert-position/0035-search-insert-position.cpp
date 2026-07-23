8b6ee5882572a6c7f107c850a2fe7b44441ca733
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-07-23 21:39:46 +0530

    Time: 0 ms (100%), Space: 13.6 MB (43.51%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target,int low ,int high){
+        if (low > high ) {
+            return low;
+        }
+        int mid = low + (high-low)/2;
+         if(nums[mid]==target){ return mid;}
+        else if (nums[mid]<target){
+            return binarySearch(nums , target ,mid+1, high);
+        }
+        else {
+            return binarySearch(nums, target ,low, mid-1);
+        }
+      
+    }
+    int searchInsert(vector<int>& nums, int target) {
+        return binarySearch( nums, target ,0, nums.size()-1);
+    }
+};
