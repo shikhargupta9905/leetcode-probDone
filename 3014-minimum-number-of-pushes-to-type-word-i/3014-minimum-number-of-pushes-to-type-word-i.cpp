2add4e4d359b689059edbcdeb59d95998d00babc
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-07-30 12:46:04 +0530

    Time: 2 ms (16.1%), Space: 8.3 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n = word.length();
-        int pushcnt = 0;
-        for(int i=0;i<n;i++){
-            pushcnt+=i/8 +1;
-
-        }
-        return pushcnt;
+        int blocks = n/8;
+        return (blocks*(blocks+1)*4)+(n%8)*(blocks+1);
     }
 };
