65b8fa56702f38e037cb5fa0b83a05184cf9305b
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-08-06 20:44:30 +0530

    Time: 185 ms (13.47%), Space: 11.1 MB (64.87%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,22 +3,32 @@ public:
     int beautySum(string s) {
         int n = s.size();
         int sumOfBeauty =0 ;
+        vector<vector<int>> freq (26, vector<int>(n, 0) );
 
         for(int i =0 ;i<n ;i++){
-            for(int j = i;j<n;j++){
-                vector<int> freq(26,0);
-                for(int k =i;k<=j; k++){
-                    freq[s[k]-'a']++;
+            for(int ch= 0;ch<26;ch++){
+                freq[ch][i] = (i>0) ? freq[ch][i-1] : 0;
                 }
-                 int maxi = INT_MIN, mini = INT_MAX;
-                 for(int i =0 ;i<26; i++){
-                    if(freq[i]==0) continue;
-                    maxi= max(maxi, freq[i]);
-                    mini = min (mini , freq[i]);
-                 }
+            freq[s[i] - 'a'][i]++;
+
+        }
+      for ( int i=0; i<n; i++ ) {
+            for ( int j=i; j<n; j++ ) {
+                int maxi = INT_MIN, mini = INT_MAX; 
+
+                  for ( int ch=0; ch<26; ch++ ) {
+                    int diff = freq[ch][j] - ( (i>0) ? freq[ch][i-1] : 0 );
+                    if ( diff > 0 ) {
+                        maxi = max ( maxi, diff );
+                        mini = min ( mini, diff );
+                    }
+                }
                  sumOfBeauty+=(maxi- mini);
             }
         }
+        
         return sumOfBeauty;
-    }
+    
+       
+        }
 };
