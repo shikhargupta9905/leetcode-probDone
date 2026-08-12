5c411685363c4f36b416607a1283166e90e0e85c
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-08-12 12:41:02 +0530

    Time: 0 ms (100%), Space: 15.9 MB (13.49%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int  n = nums.size();
-        int i = n-2;
-        while(i>=0 && nums[i]>=nums[i+1]){
-            i--;
-        }
-        if(i>=0){
-            int j = n-1;
-            while(nums[j]<= nums[i]){
-                j--;
+        int pivot=-1;
+        for(int i = n-2; i>= 0 ; i--){
+            if(nums[i]<nums[i+1]){
+                pivot=i;
+                break;
             }
-            swap(nums[i], nums[j]);
         }
-        reverse(nums.begin()+i+1, nums.end());
+       // if no pivot
+        if(pivot==-1){
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        // finding the next greater element from the pivot 
+        for(int i = n-1; i>pivot; i--){
+            if(nums[i]>nums[pivot]){
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        } 
+        reverse(nums.begin()+pivot+1, nums.end());
     }
 };
