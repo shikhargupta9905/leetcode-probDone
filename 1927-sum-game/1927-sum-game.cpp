8815f280d86fb04fd9f701112087e9056b51eda7
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-08-23 19:10:52 +0530

    Time: 7 ms (21.45%), Space: 14.1 MB (33.57%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    bool sumGame(string& num) {
-        const int n=num.size(), n2=n>>1;
-        int diff=0;
-        for(int i=0; i<n; i++){
-            const char c=num[i];
-            bool isq=c=='?', half=i<n2;
-            int sgn=(half<<1)-1;
-            int d=(-isq & 9)+(-!isq & (c-'0')<<1);
-            diff+=sgn*d;
+    bool sumGame(string num) {
+        int n = num.size();
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for(int i = 0; i < n / 2; i++) {
+            if(num[i] == '?') leftQ++;
+            else leftSum += num[i] - '0';
         }
-        return diff!=0;
+
+        for(int i = n / 2; i < n; i++) {
+            if(num[i] == '?') rightQ++;
+            else rightSum += num[i] - '0';
+        }
+
+        return (leftSum - rightSum) * 2 != (rightQ - leftQ) * 9;
     }
 };
