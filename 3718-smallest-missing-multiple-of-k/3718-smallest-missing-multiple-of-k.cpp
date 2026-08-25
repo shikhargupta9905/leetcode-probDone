24b25d7987197f655429681e16e013ad9e7bd4eb
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-08-25 19:18:55 +0530

    Time: 0 ms (100%), Space: 24.1 MB (67.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-    sort(nums.begin(),nums.end());
-    int multiple = k;
-    for(int num:nums){
-        if(num==multiple){
-            multiple+=k;
-        }
+    int max_val=0;
+    for(int num:nums) max_val= max(max_val, num);
+
+    vector<bool>present(max_val+1,false);
+    for(int num: nums) present[num]=true;
+
+    int multiple=k;
+    while(multiple<=max_val && present[multiple]){
+        multiple+=k;
     }
     return multiple;
     }
