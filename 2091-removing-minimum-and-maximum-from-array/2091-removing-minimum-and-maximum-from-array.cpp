077d6fa42181776a07bb1c3e2faee26916e32f72
Shikhar Gupta <2k24cs1o.2413514@gmail.com>
2026-08-30 20:13:28 +0530

    Time: 0 ms (100%), Space: 92.3 MB (4.89%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -2,33 +2,18 @@ class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
-        int left = 0;
-        int right = 0;
-        
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[left])
-                left = i;
-                
-            if (nums[i] > nums[right])
-                right = i;
-        }
-        
-        if (left < right)
-            swap(left, right);
-            
-        int ans = n;
-        
-        for (int i = 0; i <= n; i++) {
-            int extra = 0;
-            
-            if (right >= i)
-                extra = n - right;
-            else if (left >= i)
-                extra = n - left;
-                
-            ans = min(ans, i + extra);
-        }
-        
-        return ans;
+
+        int minIndex = min_element(nums.begin(), nums.end()) - nums.begin();
+        int maxIndex = max_element(nums.begin(), nums.end()) - nums.begin();
+
+        int left = min(minIndex, maxIndex);
+        int right = max(minIndex, maxIndex);
+
+        int front = right + 1;
+        int back = n - left;
+       
+        int frontBack = (left + 1) + (n - right);
+
+        return min({front, back, frontBack});
     }
 };
